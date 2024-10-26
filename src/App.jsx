1fe15d9883e5f765e74769795a1821b6bf4c9dcc
proabdulahad <144206282+proabdulahad@users.noreply.github.com>
2024-10-26 21:22:22 +0600

    reamde file porblem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,6 @@ import Banner from './Components/Banner/Banner';
 import Footer from './Components/Footer/Footer';
 import AvailableBtn from './Components/FunctionalSection/AvailableBtn';
 import Navber from './Components/Navber/Navber';
-<<<<<<< HEAD
-
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-
-function App() {
-  const [selectedPlayer, setSelectedPlayer] = useState([]);
-  const [playerDone, setPlayerDone]=useState(false)
-
-  // add player to selected player
-  const addPlayerToSelectedPlayer = (player, money) => {
-    console.log(money)
-    if (selectedPlayer.length <6) {
-      
-      const allReadyAdded = selectedPlayer.find(
-        addedPlayer => addedPlayer.playerId == player.playerId
-      );
-  
-      if (donateMoney <= money) {
-        toast.error(
-          'You do not have enough money to hire this player. Claim some credit',
-          { position: 'top-center' }
-        );
-        return;
-      }
-  
-      if (!allReadyAdded) {
-        setSelectedPlayer([...selectedPlayer, player]);
-        toast.success(`Congrates!! ${player.name}  is now in your team.`, {
-          position: 'top-center',
-        });
-        setDonateMondy(donateMoney - money);
-        setPlayerDone(true)
-        
-      }
-
-      // color
-
-      
-      
-    }else{
-      
-
-
-      toast('You can not select more than 6 players');
-      return;
-=======
 
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -67,7 +19,6 @@ function App() {
       
     }else{
       setSelectedPlayer([...selectedPlayer, player]);
->>>>>>> e3e5473f60d7ec58594ccdc6513ce87d47c0712b
      
     }
     console.log(selectedPlayer.length);
@@ -79,21 +30,6 @@ function App() {
       toast('This Player is already Selected', { position: 'top-center' });
     }
   };
-<<<<<<< HEAD
-
-  // add money
-  const [donateMoney, setDonateMondy] = useState(0);
-  const addMoney = () => {
-    toast.success('90000 taka added to your account', {
-      position: 'top-center',
-    });
-
-    setDonateMondy(donateMoney + 90000);
-  };
-
- 
-
-=======
 
   // add money
   const [donateMoney, setDonateMondy] = useState(0);
@@ -128,7 +64,6 @@ function App() {
     }
   };
 
->>>>>>> e3e5473f60d7ec58594ccdc6513ce87d47c0712b
   // remove
   const handleRemove = id => {
     const updatedPlayer = selectedPlayer.filter(
@@ -174,12 +109,7 @@ function App() {
         addPlayerToSelectedPlayer={addPlayerToSelectedPlayer}
         handleIsActiveState={handleIsActiveState}
         isActive={isActive}
-<<<<<<< HEAD
-        playerDone={playerDone}
-      
-=======
         reduceMoney={reduceMoney}
->>>>>>> e3e5473f60d7ec58594ccdc6513ce87d47c0712b
         handleRemove={handleRemove}
       ></AvailableBtn>
       {/* Footer Section */}
